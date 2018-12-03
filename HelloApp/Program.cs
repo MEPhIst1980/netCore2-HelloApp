4f7630a3bd05f782dde90d2afd9fe3af38078933
mephist1980 <MEPhIst1980@gmail.com>
2018-12-03 16:27:36 +0300

    Добавьте файлы проекта.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace HelloApp
+{
+    public class Program
+    {
+        //public static void Main(string[] args)
+        //{
+        //    using (var host = WebHost.Start("http://localhost:8080", async context =>
+        //    {
+        //        context.Response.ContentType = "text/html; charset=utf-8";
+        //        await context.Response.WriteAsync("Привет мир!");
+        //    }))
+        //    {
+        //        host.WaitForShutdown();
+        //    }
+        //}
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
